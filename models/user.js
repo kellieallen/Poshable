2a82e4be9e7c6fd6eb8c51c48448a596e12665b1
kellieallen <52843269+kellieallen@users.noreply.github.com>
2019-09-24 10:46:19 -0400

    changed signup alert message

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ return sequelize.define('user', {
     username: { type:DataTypes.STRING,
         unique: {
         args: true,
-        msg: 'Username already in use!'
+        msg: 'Username already in use - please choose a new one!'
               }},
 
     passwordhash: {type:DataTypes.STRING, 
