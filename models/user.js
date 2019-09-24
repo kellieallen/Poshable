72bcfaebd5cbf9b55499623cd73563c51f1891dd
kellieallen <52843269+kellieallen@users.noreply.github.com>
2019-09-24 10:52:43 -0400

    resetting tables

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ return sequelize.define('user', {
     
     username: { type:DataTypes.STRING,
         unique: {
-        args: true,
-        msg: 'Username already in use - please choose a new one!'
+            args: true,
+            msg: 'Username already in use - please choose a new one!'
               }},
 
     passwordhash: {type:DataTypes.STRING, 
